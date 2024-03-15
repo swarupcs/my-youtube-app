91a03242b0d8e1a7b43911b298fecf9b7814fa58
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-15 18:11:17 +0530

    Sidebar Toggle Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import Body from "./components/Body";
 import Head from "./components/Head";
+import store from "./utils/store";
 function App() {
   return (
+    <Provider store={store}>
+
+    
     <div>
       <Head/>
       <Body/>
     </div>
+    </Provider>
   );
 }
 /**
--- src/components/Body.js
+++ src/components/Body.js
@@ -12,3 +12,4 @@ const Body = () => {
 }
 
 export default Body
+

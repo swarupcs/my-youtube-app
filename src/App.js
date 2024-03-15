4250f7e5b9c19d074f4bda164d5d1ecfab2f1996
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-15 22:45:07 +0530

    Outlet Configuration

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import "./App.css";
 import Body from "./components/Body";
 import Head from "./components/Head";
 import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer/>,
+      }, 
+      {
+        path: "watch",
+        element: <WatchPage />
+      },
+    ]
+  },
+]);
 function App() {
   return (
     <Provider store={store}>
-
-    
-    <div>
-      <Head/>
-      <Body/>
-    </div>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter}/>
+        {/* <Body /> */}
+      </div>
     </Provider>
   );
 }
@@ -28,5 +47,3 @@ Body
  */
 
 export default App;
-
-

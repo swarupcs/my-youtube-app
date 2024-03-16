1ef007461ae01cae31dee33228ba2eb5ff7c653a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-16 14:22:18 +0530

    Implement Debouncing Technique

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  // console.log(searchQuery);
+
+  useEffect(() => {
+    // API call
+    // console.log(searchQuery);
+
+    // make an api call after every key press
+    // but if the difference between 2 API calls is <200ms
+    // decline the API call
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    console.log("API CALL - " + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    // console.log(json)
+  }
+
+
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
-    dispatch(toggleMenu()); 
-  }
+    dispatch(toggleMenu());
+  };
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-           onClick={() => toggleMenuHandler()}
+          onClick={() => toggleMenuHandler()}
           className="h-8 cursor-pointer"
           alt="menu"
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
@@ -28,8 +55,10 @@ const Head = () => {
       </div>
       <div className="col-span-10 px-10">
         <input
+          value={searchQuery}
           className="w-1/2 border border-gray-400 p-2 rounded-l-full"
           type="text"
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
           🔍

990a1089d58aed37abe63b0412b0892646a34257
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-16 15:15:25 +0530

    searchbar some fix with debounce

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,6 +6,7 @@ import { YOUTUBE_SEARCH_API } from "../utils/constants";
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(true);
   // console.log(searchQuery);
 
   useEffect(() => {
@@ -56,25 +57,29 @@ const Head = () => {
       <div className="col-span-10 px-10">
         <div>
           <input
-            value={searchQuery}
             className="px=5 w-1/2 border border-gray-400 p-2 rounded-l-full"
             type="text"
+            value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
 
-        <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
-          <ul>
-            {suggestions.map((s) => (
-              <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
-                🔍 {s}
-              </li>
-            ))}
-          </ul>
-        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );

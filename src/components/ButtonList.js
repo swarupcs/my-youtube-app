847f01762a65d746e2878fada33c2e47ac5019b3
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-18 23:02:06 +0530

    commit

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -2,23 +2,42 @@ import React from 'react'
 import Button from "./Button";
 
 const ButtonList = () => {
-  const list = ["All", "Live", "Gaming"];
+  const buttonList = ["All", "Javascript", "Java", "Live", "Music", "Songs", "Vlogs", "Trending", "Programming", "News", "Technology", "Cricket", "Comedy", "Thriller", "New to you", "Computer Programming", "Netlify", "Coding"];
   return (
     <div className="flex">
-      <Button name="All" />
-      <Button name="Gaming" />
-      <Button name="Songs" />
-      <Button name="Live" />
-      <Button name="Soccer" />
-      <Button name="Cricket" />
-      <Button name="Cooking" />
-      <Button name="Cricket" />
-      <Button name="Valentines" />
-      <Button name="Cricket" />
-      <Button name="Cooking" />
-      <Button name="Cricket" />
-    </div>
+    <Button name="All" />
+    <Button name="Gaming" />
+    <Button name="Songs" />
+    <Button name="Live" />
+    <Button name="Soccer" />
+    <Button name="Cricket" />
+    <Button name="Cooking" />
+    <Button name="Cricket" />
+    <Button name="Valentines" />
+    <Button name="Cricket" />
+    <Button name="Cooking" />
+    <Button name="Cricket" />
+  </div>
+
   )
 }
 
-export default ButtonList
+export default ButtonList;
+
+
+/**
+ * 
+ *    <div className="flex">
+        {
+          buttonList.map((buttonName, index) => {
+            return (
+              <div key={index}>
+                <button className='bg-gray-200'><span className="whitespace-nowrap">{buttonName}</span></button>
+              </div>
+            )
+          })
+        }
+    </div>
+*/
+
+
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,108 @@
 import React from 'react'
+import { CiHome } from "react-icons/ci";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+
+const sidebarItem = [
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+  {
+      icons: <CiHome size="24px" />,
+      title: "Home"
+  },
+  {
+      icons: <SiYoutubeshorts size="24px" />,
+      title: "Short"
+  },
+  {
+      icons: <MdOutlineSubscriptions size="24px" />,
+      title: "Subscription"
+  },
+]
 
 const Sidebar = () => {
   const isMenuOpen = useSelector(store => store.app.isMenuOpen);
@@ -10,29 +112,18 @@ const Sidebar = () => {
     return null;
   }
   return (
-    <div className="p-5 shadow-lg w-48">
-    <ul>
-    <Link to="/"><li> Home </li></Link>
-      
-      <li> Shorts</li>
-      <li> Videos</li>
-      <li> Live</li>
-    </ul>
-    <h1 className="font-bold pt-5">Subscriptions</h1>
-    <ul>
-      <li> Music</li>
-      <li> Sports</li>
-      <li> Gaming</li>
-      <li> Movies</li>
-    </ul>
-    <h1 className="font-bold pt-5">Watch Later</h1>
-    <ul>
-      <li> Music</li>
-      <li> Sports</li>
-      <li> Gaming</li>
-      <li> Movies</li>
-    </ul>
-  </div>
+    <div className={`relative left-0 ${isMenuOpen? "w-[20%]" : "w-[6%]"} p-5 h-[calc(100vh-4.625rem)] bg-white overflow-y-scroll overflow-x-hidden`}>
+    {
+        sidebarItem.map((item, index) => {
+            return (
+                <div key={index} className='flex my-3 ml-2'>
+                    {item.icons}
+                    <p className={`ml-5 ${isMenuOpen ? "": 'hidden'}`}>{item.title}</p>
+                </div>
+            )
+        })
+    }
+</div>
   )
 }
 

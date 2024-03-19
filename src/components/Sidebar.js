a86d33d3c89f21020191c1e71882f444945dad19
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-19 21:14:51 +0530

    sidebar fixed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,108 +1,15 @@
 import React from 'react'
-import { CiHome } from "react-icons/ci";
-import { SiYoutubeshorts } from "react-icons/si";
-import { MdOutlineSubscriptions } from "react-icons/md";
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
-
-const sidebarItem = [
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-  {
-      icons: <CiHome size="24px" />,
-      title: "Home"
-  },
-  {
-      icons: <SiYoutubeshorts size="24px" />,
-      title: "Short"
-  },
-  {
-      icons: <MdOutlineSubscriptions size="24px" />,
-      title: "Subscription"
-  },
-]
+import { CiHome, CiMusicNote1 } from "react-icons/ci";
+import { GoHome, GoStopwatch, GoThumbsup, GoVideo } from "react-icons/go";
+import { SiYoutubemusic, SiYoutubeshorts, SiYoutubestudio } from "react-icons/si";
+import { MdOutlinePodcasts, MdOutlineUserCircle, MdSubscriptions,MdOutlineSubscriptions } from "react-icons/md";
+import { FaChevronRight, FaFileVideo, FaHistory, FaUser, FaViadeoSquare, FaVideo, FaYoutube } from "react-icons/fa";
+import { HiOutlineLightBulb, HiOutlineNewspaper, HiOutlineSignal, HiOutlineTrophy, HiOutlineUserCircle } from "react-icons/hi2";
+import {AiOutlineExclamationCircle, AiOutlineFire, AiOutlineFlag, AiOutlineQuestionCircle, AiOutlineSetting, AiOutlineShopping} from "react-icons/ai"
+import {PiCoatHanger, PiFilmSlate, PiGameController} from 'react-icons/pi'
+import {TbBrandYoutubeKids} from 'react-icons/tb'
 
 const Sidebar = () => {
   const isMenuOpen = useSelector(store => store.app.isMenuOpen);
@@ -112,18 +19,50 @@ const Sidebar = () => {
     return null;
   }
   return (
-    <div className={`relative left-0 ${isMenuOpen? "w-[20%]" : "w-[6%]"} p-5 h-[calc(100vh-4.625rem)] bg-white overflow-y-scroll overflow-x-hidden`}>
-    {
-        sidebarItem.map((item, index) => {
-            return (
-                <div key={index} className='flex my-3 ml-2'>
-                    {item.icons}
-                    <p className={`ml-5 ${isMenuOpen ? "": 'hidden'}`}>{item.title}</p>
-                </div>
-            )
-        })
-    }
-</div>
+    <div className="p-5 pt-0  shadow-lg col-span-1 w-full h-[calc(100vh-4rem)] overflow-y-scroll custom-scrollbar1  mt-16 relative lg:relative">
+    <ul className='text-sm'>
+      <li className='sidebarList'><GoHome className='mr-3 text-2xl'/> Home</li>
+      <li className='sidebarList '><SiYoutubeshorts className='mr-3 text-2xl'/> Shorts</li>
+      <li className='sidebarList '><MdOutlineSubscriptions className='mr-2 text-2xl'/> Subscriptions</li>
+      <hr className='my-2' />
+      <li className='sidebarList '>You <FaChevronRight className='mr-3 text-1xl'/></li>
+      <li className='sidebarList'><HiOutlineUserCircle className='mr-3 text-2xl'/> Your Channel</li>
+      <li className='sidebarList'><FaHistory  className='mr-3 text-2xl '/> History</li>
+      <li className='sidebarList'><GoVideo  className='mr-3 text-2xl '/> Your Videos</li>
+      <li className='sidebarList'><GoStopwatch  className='mr-3 text-2xl '/> Watch Later</li>
+      <li className='sidebarList'><GoThumbsup  className='mr-3 text-2xl '/> Liked Videos</li>
+    </ul>
+    <hr className='my-2' />
+    <h1 className="font-bold">Explore</h1>
+    <ul>
+      <li className='sidebarList'><AiOutlineFire  className='mr-3 text-2xl '/> Trending</li>
+      <li className='sidebarList'><AiOutlineShopping  className='mr-3 text-2xl '/> Shopping</li>
+      <li className='sidebarList'><CiMusicNote1  className='mr-3 text-2xl '/> Music</li>
+      <li className='sidebarList'><PiFilmSlate  className='mr-3 text-2xl '/> Film</li>
+      <li className='sidebarList'><HiOutlineSignal  className='mr-3 text-2xl '/> Live</li>
+      <li className='sidebarList'><PiGameController  className='mr-3 text-2xl '/>Gaming</li>
+      <li className='sidebarList'><HiOutlineNewspaper  className='mr-3 text-2xl '/> News</li>
+      <li className='sidebarList'><HiOutlineTrophy  className='mr-3 text-2xl '/> Sports</li>
+      <li className='sidebarList'><HiOutlineLightBulb  className='mr-3 text-2xl '/> Learing</li>
+      <li className='sidebarList'><MdOutlinePodcasts  className='mr-3 text-2xl '/> Podcasts</li>
+    </ul>
+    <hr className='my-2' />
+    <h1>More from Youtube</h1>
+    <ul className='text-sm'>
+      <li className='sidebarList'><FaYoutube  className='mr-3 text-2xl  text-red-500 '/> Youtube Premium</li>
+      <li className='sidebarList'><SiYoutubestudio  className='mr-3 text-2xl  text-red-500'/> Youtube Studio</li>
+      <li className='sidebarList'><SiYoutubemusic  className='mr-3 text-2xl  text-red-500 '/> Youtube Music</li>
+      <li className='sidebarList'><TbBrandYoutubeKids  className='mr-3 text-2xl  text-red-500 '/> Youtube Kids</li>
+    </ul>
+    <hr className='my-2' />
+    <ul className='text-sm'>
+      <li className='sidebarList'><AiOutlineSetting  className='mr-3 text-2xl '/> Settings</li>
+      <li className='sidebarList'><AiOutlineFlag  className='mr-3 text-2xl '/>Request history</li>
+      <li className='sidebarList'><AiOutlineQuestionCircle  className='mr-3 text-2xl '/> Help</li>
+      <li className='sidebarList'><AiOutlineExclamationCircle  className='mr-3 text-2xl '/> Send Feedback</li>
+
+    </ul>
+  </div>
   )
 }
 
